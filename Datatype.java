20fe91641183b371b1dc038e51394eff513e8d6a
vanshaggrwal <101628876+vanshaggrwal@users.noreply.github.com>
2024-10-14 14:42:49 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+
+
+class Datatype {
+    public static void main(String[] args) {
+        
+          int intVal = 2147483647;
+        long longVal = 9223372036854775807L;
+        float floatVal = 3.14f;
+        double doubleVal = 3.14159265359;
+        char charVal = 'A';
+        boolean boolVal = true;
+      System.out.println("INT VALUE: "+intVal);
+       System.out.println("Long VALUE: "+longVal);
+       System.out.println("FLoat VALUE: "+floatVal);
+       System.out.println("DoUBLE VALUE: "+doubleVal);
+       System.out.println("Char VALUE: "+charVal);
+        System.out.println("BOOLEAN VALUE: "+boolVal);
+    }
+}
